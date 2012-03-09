99b7051cd48a47361277e68c1707c1104de3681e
Vegar Engen <vegar.engen@gmail.com>
2012-03-09 15:27:01 +0100

    fixed .gitignore

--- GoldMine/gen/no/ntnu/tdt4240/R.java
+++ GoldMine/gen/no/ntnu/tdt4240/R.java
@@ -17,9 +17,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int about_button=0x7f060001;
-        public static final int exit_button=0x7f060002;
-        public static final int new_button=0x7f060000;
+        public static final int about_button=0x7f060002;
+        public static final int exit_button=0x7f060003;
+        public static final int linearLayout1=0x7f060000;
+        public static final int new_button=0x7f060001;
     }
     public static final class layout {
         public static final int game=0x7f030000;
