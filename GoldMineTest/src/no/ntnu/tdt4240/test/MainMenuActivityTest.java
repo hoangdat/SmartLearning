70e521d524c7c03af42a34cca511c5630e2521dc
Vegar Engen <vegar.engen@gmail.com>
2012-03-09 14:36:11 +0100

    Added some tests

--- GoldMineTest/src/no/ntnu/tdt4240/test/MainMenuActivityTest.java
+++ GoldMineTest/src/no/ntnu/tdt4240/test/MainMenuActivityTest.java
@@ -1,7 +1,31 @@
 package no.ntnu.tdt4240.test;
 
-import android.test.ActivityTestCase;
+import no.ntnu.tdt4240.MainMenuActivity;
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 
-public class MainMenuActivityTest extends ActivityTestCase{
+public class MainMenuActivityTest extends ActivityInstrumentationTestCase2<MainMenuActivity>{
 
+	private MainMenuActivity mActivity;
+	private TextView mView;
+	private Object resourceString;
+
+	public MainMenuActivityTest() {
+		super("no.ntnu.tdt4240.MainMenuActivity", MainMenuActivity.class);
+	}
+	
+	@Override
+	protected void setUp() throws Exception {
+		super.setUp();
+		mActivity = getActivity();
+		mView = (TextView) mActivity.findViewById(no.ntnu.tdt4240.R.id.main_title);
+		resourceString = mActivity.getString(no.ntnu.tdt4240.R.string.hello);
+	}
+	
+	public void testPrecondition() {
+		assertNotNull(mView);
+	}
+	public void testText() {
+		assertEquals(resourceString, (String)mView.getText());
+	}
 }
