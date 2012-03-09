5ecdc067307e3e19e2cdd07928fe0016021e56f7
Vegar Engen <vegar.engen@gmail.com>
2012-03-09 13:06:11 +0100

    Init commit

---
+++ GoldMine/src/no/ntnu/tdt4240/GoldMineActivity.java
@@ -0,0 +1,13 @@
+package no.ntnu.tdt4240;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class GoldMineActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
