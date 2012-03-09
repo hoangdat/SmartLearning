3cfe44916289a1a24d0e664a88611e7af0ac8a71
Vegar Engen <vegar.engen@gmail.com>
2012-03-09 14:14:02 +0100

    Activity name and notification bar is hidden

--- GoldMine/gen/no/ntnu/tdt4240/R.java
+++ GoldMine/gen/no/ntnu/tdt4240/R.java
@@ -10,6 +10,9 @@ package no.ntnu.tdt4240;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int background=0x7f040000;
+    }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
@@ -17,7 +20,8 @@ public final class R {
         public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
+        public static final int app_name=0x7f050001;
+        public static final int hello=0x7f050000;
+        public static final int main_title=0x7f050002;
     }
 }
--- GoldMine/src/no/ntnu/tdt4240/MainMenuActivity.java
+++ GoldMine/src/no/ntnu/tdt4240/MainMenuActivity.java
@@ -2,6 +2,8 @@ package no.ntnu.tdt4240;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainMenuActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,7 +11,12 @@ public class MainMenuActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         
-        
+        // hide title
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        // Hide notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.main);
         
     }
