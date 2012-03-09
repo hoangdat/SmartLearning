e1007262d0f0e2422d337d2047c01b768d85ae91
Jonas Olseng <jonas_olseng@hotmail.com>
2012-03-09 14:55:02 +0100

    xml

--- GoldMine/gen/no/ntnu/tdt4240/R.java
+++ GoldMine/gen/no/ntnu/tdt4240/R.java
@@ -17,11 +17,9 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int about_button=0x7f060003;
-        public static final int exit_button=0x7f060004;
-        public static final int main_title=0x7f060001;
-        public static final int new_button=0x7f060002;
-        public static final int title_menu=0x7f060000;
+        public static final int about_button=0x7f060001;
+        public static final int exit_button=0x7f060002;
+        public static final int new_button=0x7f060000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
