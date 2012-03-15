f5d429cd02d15c242c0fd5cd010c616c24bd91b1
Kjeks <randomguy@randomguy.com>
2012-03-15 13:53:52 +0100

    fiksa board

--- GoldMine/src/no/ntnu/tdt4240/models/Mine.java
+++ GoldMine/src/no/ntnu/tdt4240/models/Mine.java
@@ -1,10 +1,5 @@
 package no.ntnu.tdt4240.models;
-
-
-
 import no.ntnu.tdt4240.R;
-import no.ntnu.tdt4240.activities.GameActivity;
-
 import android.content.Context;
 
 public class Mine extends Cell {
@@ -13,7 +8,7 @@ public class Mine extends Cell {
 	
 	Mine(Context context) {
 		super(context);
-//		this.setBackgroundResource(R.drawable......)
+		//this.setBackgroundResource(R.drawable)
 		clicked = false;
 	}
 	
