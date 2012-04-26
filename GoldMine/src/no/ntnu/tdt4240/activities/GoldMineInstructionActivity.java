1d359c8a002213a12f66706f9aaea6518d2605ae
Jonas Olseng <jonas_olseng@hotmail.com>
2012-04-26 21:28:19 +0200

    instructiontab

---
+++ GoldMine/src/no/ntnu/tdt4240/activities/GoldMineInstructionActivity.java
@@ -0,0 +1,13 @@
+package no.ntnu.tdt4240.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import no.ntnu.tdt4240.R;
+
+public class GoldMineInstructionActivity extends Activity{
+	public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.goldmineinstructions_layout);
+    }
+	
+}
--- GoldMine/src/no/ntnu/tdt4240/activities/InstructionsActivity.java
+++ GoldMine/src/no/ntnu/tdt4240/activities/InstructionsActivity.java
@@ -1,14 +1,38 @@
 package no.ntnu.tdt4240.activities;
 
 import no.ntnu.tdt4240.R;
-import android.app.Activity;
+import android.app.TabActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
 
-public class InstructionsActivity extends Activity {
+public class InstructionsActivity extends TabActivity {
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.instructions);
-	}
+		 
+        TabHost tabHost = getTabHost();
+ 
+        // Tab for Photos
+        TabSpec puregoldspec = tabHost.newTabSpec("Pure Gold");
+        // setting Title and Icon for the Tab
+        puregoldspec.setIndicator("Pure Gold", getResources().getDrawable(R.drawable.gold));
+        Intent photosIntent = new Intent(this, PureGoldInstructionActivity.class);
+        puregoldspec.setContent(photosIntent);
+ 
+        // Tab for Songs
+        TabSpec goldminespec = tabHost.newTabSpec("GoldMine");
+        goldminespec.setIndicator("GoldMine", getResources().getDrawable(R.drawable.wormsmine));
+        Intent songsIntent = new Intent(this, GoldMineInstructionActivity.class);
+        goldminespec.setContent(songsIntent);
+ 
+ 
+        // Adding all TabSpec to TabHost
+        tabHost.addTab(puregoldspec); 
+        tabHost.addTab(goldminespec); 
+        
+    }
 }
