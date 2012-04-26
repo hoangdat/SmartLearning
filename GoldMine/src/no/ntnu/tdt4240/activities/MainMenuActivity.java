1853cc95ae7379d7adf38c19ca81ea5d60ccbce3
Vegar Engen <vegar.engen@akuma>
2012-04-26 14:44:25 +0200

    Some stash

--- GoldMine/src/no/ntnu/tdt4240/activities/MainMenuActivity.java
+++ GoldMine/src/no/ntnu/tdt4240/activities/MainMenuActivity.java
@@ -4,39 +4,55 @@ import no.ntnu.tdt4240.R;
 import no.ntnu.tdt4240.sound.BackgroundMusic;
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MainMenuActivity extends Activity implements android.view.View.OnClickListener {
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // Hide title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        // Hide notification bar
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.main);
-       
-        // Click-handlers for buttons
-        View newgameButton = findViewById(R.id.new_button);
-        newgameButton.setOnClickListener(this);
-        View settingsButton = findViewById(R.id.settings_button);
-        settingsButton.setOnClickListener(this); 
-        View instructionsButton = findViewById(R.id.instructions_button);
-        instructionsButton.setOnClickListener(this);
-        View aboutButton = findViewById(R.id.about_button);
-        aboutButton.setOnClickListener(this);
-        View exitButton = findViewById(R.id.exit_button);
-        exitButton.setOnClickListener(this);
-        
-        BackgroundMusic.loadSound(this);
-    	BackgroundMusic.playMusic();
-        
-    }
+public class MainMenuActivity extends Activity implements
+		android.view.View.OnClickListener {
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// Hide title
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		// Hide notification bar
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		setContentView(R.layout.main);
+
+		// Click-handlers for buttons
+		TextView newgameButton = (TextView) findViewById(R.id.new_button);
+		TextView settingsButton = (TextView) findViewById(R.id.settings_button);
+		TextView instructionsButton = (TextView) findViewById(R.id.instructions_button);
+		TextView aboutButton = (TextView) findViewById(R.id.about_button);
+		TextView exitButton = (TextView) findViewById(R.id.exit_button);
+
+		initiateMenuElement(newgameButton);
+		initiateMenuElement(settingsButton);
+		initiateMenuElement(instructionsButton);
+		initiateMenuElement(aboutButton);
+		initiateMenuElement(exitButton);
+		
+		
+		
+		BackgroundMusic.loadSound(this);
+		BackgroundMusic.playMusic();
+	}
+
+	private void initiateMenuElement(TextView textButton) {
+		Typeface tf = Typeface.createFromAsset(getAssets(), "font/baveuse.otf");
+		textButton.setTextColor(getResources().getColor(R.color.mainmenu));
+		textButton.setOnClickListener(this);
+		textButton.setTypeface(tf);
+		textButton.setTextSize(40);
+	}
 
 	public void onClick(View v) {
 		switch (v.getId()) {
@@ -57,5 +73,5 @@ public class MainMenuActivity extends Activity implements android.view.View.OnCl
 			break;
 		}
 	}
-    
+	
 }
