d267b1d099563d6318374d791ede947aae57f938
unknown <Administrator@zen-san.(none)>
2012-04-26 13:31:47 +0200

    mer lyd

--- GoldMine/src/no/ntnu/tdt4240/sound/BackgroundMusic.java
+++ GoldMine/src/no/ntnu/tdt4240/sound/BackgroundMusic.java
@@ -10,7 +10,10 @@ import android.media.SoundPool;
 public class BackgroundMusic{
 	private static SoundPool soundPool;
 	private static int mine;
-	private static int gold;
+	private static int gold1;
+	private static int gold2;
+	private static int gold3;
+	private static int gold4;
 	private static int blank;
 	private static MediaPlayer music;
 	private static boolean mute = false;
@@ -19,7 +22,10 @@ public class BackgroundMusic{
 		mute = SettingsActivity.isMuted(context); // should there be sound?
 		soundPool = new SoundPool(5, AudioManager.STREAM_MUSIC, 0);
 		mine = soundPool.load(context, R.raw.mine, 1);
-		gold = soundPool.load(context, R.raw.gold, 1);
+		gold1 = soundPool.load(context, R.raw.gold1, 1);
+		gold2 = soundPool.load(context, R.raw.gold2, 1);
+		gold3 = soundPool.load(context, R.raw.gold3, 1);
+		gold4 = soundPool.load(context, R.raw.gold4, 1);
 		blank = soundPool.load(context, R.raw.blank, 1);
 		music = MediaPlayer.create(context, R.raw.backgroundmusic);
 	}
@@ -28,8 +34,15 @@ public class BackgroundMusic{
 		if (mute) return; // if sound is turned off no need to continue
 		if (soundName.equals("mine"))
 			soundPool.play(mine, 1, 1, 1, 0, 1);
-		if (soundName.equals("gold"))
-			soundPool.play(gold, 1, 1, 1, 0, 1);
+		if (soundName.equals("gold")){
+			double random = Math.random(); //we want four different sounds when you hit gold, each in the right key
+			int goldNumber = gold1;
+			if(random <= .25) goldNumber = gold1;
+			if(random > .25 && random <= .5) goldNumber = gold2;
+			if(random > .50 && random <= .75) goldNumber = gold3;
+			if(random > .75 && random <= 1) goldNumber = gold4;
+			soundPool.play(goldNumber, 1, 1, 1, 0, 1);
+		}
 		if (soundName.equals("blank"))
 			soundPool.play(blank, 1, 1, 1, 0, 1);
 	}
